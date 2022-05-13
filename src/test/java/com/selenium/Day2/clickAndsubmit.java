314e704b87a22906420b82a05ab14a8d598cd503
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-05-13 10:12:28 -0400

    Selenium Day 2 Snippets

---
+++ src/test/java/com/selenium/Day2/clickAndsubmit.java
@@ -0,0 +1,58 @@
+package com.selenium.Day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class clickAndsubmit {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void SetUp() {
+
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
+        driver.get("https://the-internet.herokuapp.com/login");
+
+    }
+
+    @Test
+    public void click() {
+
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+
+        driver.findElement(By.className("radius")).click();
+
+        driver.navigate().back();
+
+    }
+
+    @Test
+    public void submit() {
+        WebElement cl = driver.findElement(By.id("username")); 
+        cl.clear(); 
+        cl.sendKeys("tomsmith");
+        WebElement pwd = driver.findElement(By.name("password"));
+        pwd.clear();        
+        pwd.sendKeys("SuperSecretPassword!");
+
+        driver.findElement(By.className("radius")).submit();
+        
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.findElement(By.xpath("//a[@class='button secondary radius']")).click();
+        driver.quit();
+        
+    }
+
+
+
+}
--- src/test/java/com/selenium/Day2/passData.java
+++
@@ -1,7 +0,0 @@
-package com.selenium.Day2;
-
-public class passData {
-
-    // Passing data into a form without using sendkeys 
-    
-}
