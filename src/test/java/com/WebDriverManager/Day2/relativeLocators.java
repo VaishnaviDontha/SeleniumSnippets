bb3d2cf9bd392e9dc7e18eabaa4b6a96132455fb
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-05-31 15:26:34 -0400

    WebDriver Manager Snippets

--- src/test/java/com/WebDriverManager/Day2/relativeLocators.java
+++ src/test/java/com/WebDriverManager/Day2/relativeLocators.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.locators.RelativeLocator;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class relativeLocators {
 
     WebDriver driver;
@@ -14,8 +16,10 @@ public class relativeLocators {
     @BeforeTest
     public void SetUp() {
 
+        WebDriverManager.chromedriver().setup();
+
         driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
+        // System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
         driver.get("https://the-internet.herokuapp.com/");
 
     }
