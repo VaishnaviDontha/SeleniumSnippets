7ce87113a64dc21029a38e461be3b01cad3ce6a7
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-05-31 15:28:26 -0400

    WebDriver Manager Snippets

--- src/test/java/com/WebDriverManager/Day2/usingKeys.java
+++ src/test/java/com/WebDriverManager/Day2/usingKeys.java
@@ -8,6 +8,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class usingKeys {
 
 
@@ -27,8 +29,10 @@ public class usingKeys {
     @BeforeTest
     public void SetUp() {
 
+        WebDriverManager.chromedriver().setup();
+
         driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
+        // System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
         driver.get("https://opensource-demo.orangehrmlive.com/");
         
     }
