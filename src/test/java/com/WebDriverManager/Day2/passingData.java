2e85e7f63e98a361512186f177f7a8d844cc1391
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-05-31 15:26:24 -0400

    WebDriver Manager Snippets

--- src/test/java/com/WebDriverManager/Day2/passingData.java
+++ src/test/java/com/WebDriverManager/Day2/passingData.java
@@ -8,6 +8,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class passingData {
 
     /*
@@ -20,8 +22,10 @@ public class passingData {
     @BeforeTest
     public void SetUp() {
 
+        WebDriverManager.chromedriver().setup();
+
         driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
+        // System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
         driver.get("https://the-internet.herokuapp.com/login");
     }
 
