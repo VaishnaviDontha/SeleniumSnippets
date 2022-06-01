b8adfc9c6132ba1237c83dc182cef13d6fb9e672
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-01 14:46:43 -0400

    Checkbox snippet

---
+++ src/test/java/com/selenium/Day4/checkboxes.java
@@ -0,0 +1,52 @@
+package com.selenium.Day4;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class checkboxes {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void Setup() {
+
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+
+    }
+
+    @Test
+    public void checkbox() {
+
+        if(! driver.findElement(By.xpath("(//input[@type='checkbox'])[1]")).isSelected()){
+
+            driver.findElement(By.xpath("(//input[@type='checkbox'])[1]")).click();
+
+        }
+
+        else 
+
+            driver.findElement(By.xpath("(//input[@type='checkbox'])[1]")).click(); 
+        
+        
+    }
+
+    @AfterTest
+    public void tearDown() {
+
+        driver.quit();
+        
+    }
+    
+}
