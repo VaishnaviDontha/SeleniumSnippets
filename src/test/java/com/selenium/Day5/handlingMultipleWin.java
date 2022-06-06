94e66ee49f707e6e41185e184d2080cd6475c694
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-06 18:20:29 -0400

    SE snippets

--- src/test/java/com/selenium/Day5/handlingMultipleWin.java
+++ src/test/java/com/selenium/Day5/handlingMultipleWin.java
@@ -1,5 +1,43 @@
 package com.selenium.Day5;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class handlingMultipleWin {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        
+        driver = new ChromeDriver();
+        
+        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("https://the-internet.herokuapp.com/");
+    }
+
+    @Test
+    public void multipleWindows() {
+
+        String parentGUID = driver.getWindowHandle();
+        System.out.println(parentGUID);
+        
+    }
+
+    @AfterTest
+    public void tearDown() {
+
+        driver.quit();
+        
+    }
     
 }
