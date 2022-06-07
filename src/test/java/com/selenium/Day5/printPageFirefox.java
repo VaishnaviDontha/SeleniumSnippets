47de41bc4c2055c5d22cefad94151e38b5bfa890
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-07 13:21:14 -0400

    SE Snippets

--- src/test/java/com/selenium/Day5/printPageFirefox.java
+++ src/test/java/com/selenium/Day5/printPageFirefox.java
@@ -4,15 +4,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Pdf;
 import org.openqa.selenium.PrintsPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.print.PrintOptions;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -25,10 +26,8 @@ public class printPageFirefox {
     public void setUp() {
         
         System.setProperty("webdriver.gecko.driver", "./resources/geckodriver");
-        FirefoxOptions options = new FirefoxOptions();
-        options.addArguments("--headless", "--disable-gpu");
-        FirefoxDriver driver = new FirefoxDriver(options);
 
+        driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
@@ -38,10 +37,21 @@ public class printPageFirefox {
     @Test
     public void printPage() throws IOException {
 
-        Path printPage = Paths.get("./resources/PrintPageChrome.pdf");
-        Pdf print = ((PrintsPage) driver).print(new PrintOptions());
-        Files.write(printPage, OutputType.BYTES.convertFromBase64Png(print.getContent()));
-        driver.quit();
+        // Create object of SimpleDateFormat class and decide the format
+        DateFormat dateFormat = new SimpleDateFormat("MM:dd:yyyy_HH:mm:ss");
         
+        //get current date time with Date()
+        Date date = new Date();
+        
+        // Now format the date
+        String date1= dateFormat.format(date);
+
+        // Appending timestamp to the path
+        Path printPage = Paths.get("./resources/PrintPageFirefox_"+date1+".pdf");
+        Pdf print = ((PrintsPage) driver).print(new PrintOptions());
+        Files.write(printPage, OutputType.BYTES.convertFromBase64Png(print.getContent()));        
+                
+        driver.quit();
     }
+        
 }
