c0b9f9161af5b6de4955b0b689a24330cc86aa6f
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-07 14:04:04 -0400

    SE Snippets

--- src/test/java/com/selenium/Day5/captureTimestamp.java
+++ src/test/java/com/selenium/Day5/captureTimestamp.java
@@ -1,5 +1,45 @@
 package com.selenium.Day5;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.testng.annotations.Test;
+
 public class captureTimestamp {
-    
+
+
+    @Test
+    public void TimestampAndAppend() throws IOException {
+
+        // Create object of SimpleDateFormat class and decide the format
+        DateFormat dateFormat = new SimpleDateFormat("MM:dd:yyyy_HH:mm:ss");
+
+        // or
+        DateFormat dateFormat2 = new SimpleDateFormat("MM/dd/yyyy ");
+
+        // OR
+        DateFormat dateFormat3 = new SimpleDateFormat("dd");
+
+        // OR
+        DateFormat dateFormat4 = new SimpleDateFormat("MM");
+
+        
+        //get current date time with Date()
+        Date date = new Date();
+        
+        // Now format the date
+        String date1 = dateFormat.format(date);
+        String date2 = dateFormat2.format(date);
+        String date3 = dateFormat3.format(date);
+        String date4 = dateFormat4.format(date);
+
+        System.out.println(date1);
+        System.out.println(date2);
+        System.out.println(date3);
+        System.out.println(date4); 
+        
+    }
+
 }
