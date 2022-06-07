f6da18f32fe6cd4c9b1233ed8b5cb6bdd949574a
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-07 13:31:50 -0400

    SE Snippets

--- src/test/java/com/selenium/Day3/chromeOptions.java
+++ src/test/java/com/selenium/Day3/chromeOptions.java
@@ -4,12 +4,10 @@ import java.io.File;
 import java.time.Duration;
 
 import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class chromeOptions {
