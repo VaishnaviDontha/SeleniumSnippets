dfba401d4006642d341d0774c292edda0d082730
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-09 15:40:19 -0400

    SE Snippets

--- src/test/java/com/selenium/Day5/switchTabs.java
+++ src/test/java/com/selenium/Day5/switchTabs.java
@@ -1,5 +1,149 @@
 package com.selenium.Day5;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class switchTabs {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
+        driver.get("https://the-internet.herokuapp.com/");
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        
+    }
+
+    @Test
+    public void switchTwoDiffTabs() throws InterruptedException {
+
+        Set<String> allGUID = driver.getWindowHandles();
+        String pGUID = driver.getWindowHandle();
+        System.out.println(pGUID);
+        
+        WebElement element = driver.findElement(By.cssSelector("a[href='/abtest']"));
+        
+        Actions act = new Actions(driver);
+        act.keyDown(Keys.COMMAND)
+            .click(element)
+            .build()
+            .perform();
+
+        for(String guid : allGUID){
+
+            if(guid.equals(pGUID)){
+                driver.switchTo().window(pGUID);
+                System.out.println(pGUID);
+                break;
+            }
+        }
+
+    }
+
+    @Test
+    public void switchMultipleDiffTabs() throws InterruptedException {
+        
+        WebElement element = driver.findElement(By.cssSelector("a[href='/abtest']"));
+        
+        Actions act = new Actions(driver);
+        act.keyDown(Keys.COMMAND)
+            .click(element)
+            .build()
+            .perform();
+
+        WebElement element1 = driver.findElement(By.cssSelector("a[href='/add_remove_elements/']"));
+        
+        Actions act1 = new Actions(driver);
+        act1.keyDown(Keys.COMMAND)
+            .click(element1)
+            .build()
+            .perform();
+
+        WebElement element2 = driver.findElement(By.cssSelector("a[href='/broken_images']"));
+        
+        Actions act2 = new Actions(driver);
+        act2.keyDown(Keys.COMMAND)
+            .click(element2)
+            .build()
+            .perform();
+
+        ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
+
+        driver.switchTo().window(tabs.get(2));
+
+    }
+
+
+
+    @Test(enabled = false)
+    public void openingLinkInNewWindow() throws InterruptedException {
+
+        WebElement element = driver.findElement(By.cssSelector("a[href='/abtest']"));
+        
+        Actions act = new Actions(driver);
+        act.keyDown(Keys.SHIFT)
+            .click(element)
+            .build()
+            .perform();
+        
+    }
+
+
+    @Test (enabled = false)
+    public void openingLinkInNewTab() throws InterruptedException {
+
+        WebElement element = driver.findElement(By.cssSelector("a[href='/abtest']"));
+        
+        Actions act = new Actions(driver);
+        act.keyDown(Keys.COMMAND)
+            .click(element)
+            .build()
+            .perform();
+        
+    }
+
+    @Test (enabled = false)
+    public void otherWaysofSwitchingTabs() {
+
+        WebElement element = driver.findElement(By.cssSelector("a[href='/abtest']"));
+        
+        Actions act = new Actions(driver);
+     
+        act.keyDown(Keys.COMMAND)
+            .click(element)
+            .build()
+            .perform();
+
+        ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1)); // Switches to new Tab
+        driver.switchTo().window(tabs.get(0)); // Switches to main Tab
+
+
+        
+    }
+
+
+    @AfterTest
+    public void tearDown() {
+
+        driver.quit();
+        
+    }
     
 }
