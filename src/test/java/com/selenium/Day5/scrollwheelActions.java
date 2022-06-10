bc3ace64820156d48521752f8cd839bd614ceef1
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-10 13:49:23 -0400

    SE Snippets

--- src/test/java/com/selenium/Day5/scrollwheelActions.java
+++ src/test/java/com/selenium/Day5/scrollwheelActions.java
@@ -1,5 +1,28 @@
 package com.selenium.Day5;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class scrollwheelActions {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
+        driver.get("https://the-internet.herokuapp.com/");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(1));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+
+    }
+
+
     
 }
