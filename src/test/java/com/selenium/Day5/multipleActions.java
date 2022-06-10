4cfce36aa035869b712ca16f24dd3fa14f0c9946
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-10 16:34:31 -0400

    SE Snippets

--- src/test/java/com/selenium/Day5/multipleActions.java
+++ src/test/java/com/selenium/Day5/multipleActions.java
@@ -1,5 +1,58 @@
 package com.selenium.Day5;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class multipleActions {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(1));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+
+    }
+
+    @Test
+    public void multiple() {
+
+        WebElement textField = driver.findElement(By.cssSelector("#username"));
+        new Actions(driver)
+                .sendKeys(textField, "Selenium!")
+                .sendKeys(Keys.ARROW_LEFT)
+                .sendKeys(Keys.SHIFT)
+                .sendKeys(Keys.ARROW_UP)
+                .sendKeys(Keys.COMMAND)
+                .sendKeys("xvv")
+                .perform();
+        
+    }
+
+    @AfterTest
+    public void tearDown() {
+
+        driver.quit();
+        
+    }
+
+
+
+
     
 }
