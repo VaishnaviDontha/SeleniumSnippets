a39b5552bdebe8bd0c1cd5c8c4499c1f646224d3
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-12 16:31:26 -0400

    SE Snippets

---
+++ src/test/java/com/selenium/Day6/ApachePOI/retrievingValues.java
@@ -0,0 +1,14 @@
+package com.selenium.Day6.ApachePOI;
+
+import org.testng.annotations.Test;
+
+public class retrievingValues {
+
+    @Test
+    public void retrieveValues() {
+
+        
+        
+    }
+    
+}
