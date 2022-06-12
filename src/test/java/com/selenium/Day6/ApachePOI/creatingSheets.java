4b1d5be9fbc33dc3e97f620ed2bf7380d134c3dc
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-12 16:31:02 -0400

    SE Snippets

---
+++ src/test/java/com/selenium/Day6/ApachePOI/creatingSheets.java
@@ -0,0 +1,31 @@
+package com.selenium.Day6.ApachePOI;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+public class creatingSheets {
+
+    @Test
+    public void createSheet() throws IOException {
+
+        Workbook wb = new XSSFWorkbook();
+        OutputStream file = new FileOutputStream("./resources/data.xlsx");
+
+        Sheet sheetOne = wb.createSheet("Username");
+        Sheet sheetTwo = wb.createSheet("Password");
+        Sheet sheetThree = wb.createSheet("Notes");
+
+        int numOfSheets = wb.getNumberOfSheets();
+        System.out.println(numOfSheets);
+
+        wb.write(file);
+        
+    }
+    
+}
