9549f4b573de91d975f022ba595e77093c90cdef
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-13 10:16:02 -0400

    WDM Snippets

--- src/test/java/com/WebDriverManager/Day5/keyboardActions.java
+++ src/test/java/com/WebDriverManager/Day5/keyboardActions.java
@@ -4,11 +4,9 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chromium.ChromiumDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
