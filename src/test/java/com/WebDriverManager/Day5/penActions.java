153631ca9c8c684e149d49c406330386286e4b45
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-13 10:16:21 -0400

    WDM Snippets

--- src/test/java/com/WebDriverManager/Day5/penActions.java
+++ src/test/java/com/WebDriverManager/Day5/penActions.java
@@ -31,7 +31,7 @@ public class penActions {
     }
 
     @Test
-    public void penActions() {
+    public void penAction() {
 
         WebElement pointerArea = driver.findElement(By.xpath("//a[text()='A/B Testing']"));
         
