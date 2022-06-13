a8fe67a0e64df426f6487d4d133ad64c0d4a3e24
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-13 15:20:32 -0400

    SE Snippets

---
+++ src/test/java/com/selenium/Day6/ApachePOI/readExcelSheetNum.java
@@ -0,0 +1,55 @@
+package com.selenium.Day6.ApachePOI;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
+public class readExcelSheetNum {
+
+    @Test
+    public void readingExcel() throws IOException {
+
+        String path = "/Users/harishkasam/Documents/Vaishnavi/Practise-Repo/snippets/resources/Tests.xls";
+
+        FileInputStream inputStream = new FileInputStream(path);
+
+        String fileExtensionName = path.substring(path.indexOf("."));
+        System.out.println(fileExtensionName);
+
+        if (fileExtensionName.equals(".xlsx")) {
+
+            XSSFWorkbook wb = new XSSFWorkbook(inputStream);
+            XSSFSheet sheet = wb.getSheet("Sheet1");
+            Row row = sheet.getRow(0);
+            Cell cell = row.getCell(0);
+
+            System.out.println(cell);
+            System.out.println(sheet.getRow(0).getCell(0));
+
+            wb.close();
+
+        }
+
+        else if (fileExtensionName.equals(".xls")) {
+
+            HSSFWorkbook wb = new HSSFWorkbook(inputStream);
+            HSSFSheet sheet = wb.getSheetAt(0);
+
+            Row row = sheet.getRow(0);
+            Cell cell = row.getCell(0);
+
+            System.out.println(cell);
+            System.out.println(sheet.getRow(0).getCell(0));
+
+            wb.close();
+        }
+
+    }
+}
