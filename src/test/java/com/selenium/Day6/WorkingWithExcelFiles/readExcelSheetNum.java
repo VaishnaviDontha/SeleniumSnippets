a5939a97a94bc21a6cc6bb344a693c26cb13282e
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-14 18:17:26 -0400

    Automating Excel Files

---
+++ src/test/java/com/selenium/Day6/WorkingWithExcelFiles/readExcelSheetNum.java
@@ -0,0 +1,70 @@
+package com.selenium.Day6.WorkingWithExcelFiles;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
+public class readExcelSheetNum {
+
+    @Test
+    public void readingExcel() throws IOException {
+
+        String path = "/Users/harishkasam/Documents/Vaishnavi/Practise-Repo/snippets/resources/Tests.xls";
+
+        FileInputStream inputStream = new FileInputStream(path);
+
+        String fileExtensionName = path.substring(path.indexOf("."));
+        System.out.println(fileExtensionName);
+
+        if (fileExtensionName.equals(".xlsx")) {
+
+            XSSFWorkbook wb = new XSSFWorkbook(inputStream);
+            XSSFSheet sheet = wb.getSheet("Sheet1");
+
+            int lastRow = sheet.getLastRowNum();
+
+            for(int row =0;row<= lastRow; row++){
+
+                for (int col = 0; col <= lastRow; col++) {
+
+                    Row r = sheet.getRow(row);
+                    Cell c = r.getCell(col);
+
+                    System.out.println(sheet.getRow(row).getCell(col));
+                }
+
+            wb.close();
+
+        }
+    }
+
+        else if (fileExtensionName.equals(".xls")) {
+
+            HSSFWorkbook wb = new HSSFWorkbook(inputStream);
+            HSSFSheet sheet = wb.getSheet("Username");
+
+            int lastRow = sheet.getLastRowNum();
+
+            for(int row =0;row<= lastRow; row++){
+
+                for (int col = 0; col <= lastRow; col++) {
+
+                    Row r = sheet.getRow(row);
+                    Cell c = r.getCell(col);
+
+                    System.out.println(sheet.getRow(row).getCell(col));
+                }
+            
+            }
+
+            wb.close();
+        }
+    }
+}
