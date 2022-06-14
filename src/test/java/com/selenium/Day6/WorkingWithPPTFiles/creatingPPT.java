5777db5ec2d4d0484db853bdca1ad711d677736f
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-14 18:18:26 -0400

    Automating PPT Files

---
+++ src/test/java/com/selenium/Day6/WorkingWithPPTFiles/creatingPPT.java
@@ -0,0 +1,12 @@
+package com.selenium.Day6.WorkingWithPPTFiles;
+
+import org.testng.annotations.Test;
+
+public class creatingPPT {
+
+    @Test
+    public void createPPT() {
+        
+    }
+    
+}
