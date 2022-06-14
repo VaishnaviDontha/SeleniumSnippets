e4d0f5194e78193d61b0a9639987d3503afbbd61
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-14 17:03:35 -0400

    SE Snippets

--- src/test/java/com/selenium/Day6/log4J.java
+++ src/test/java/com/selenium/Day6/log4J.java
@@ -1,5 +1,31 @@
 package com.selenium.Day6;
 
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
 public class log4J {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver");
+        driver.get("https://the-internet.herokuapp.com/login");
+        
+    }
+
+    public void log4j() {
+
+        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("tomsmith");
+        driver.findElement(By.xpath("//input[type='password']")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button[type='submit']")).submit();
+
+        
+    }
     
 }
