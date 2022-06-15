52a0e16ab8fbcdbb2b5fb2c3c6afe5b5b49dc836
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-15 17:02:33 -0400

    SE Snippets

--- src/test/java/com/selenium/Day6/log4J.java
+++ src/test/java/com/selenium/Day6/log4J.java
@@ -1,9 +1,13 @@
 package com.selenium.Day6;
 
+import java.util.logging.LogManager;
+
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class log4J {
 
@@ -24,6 +28,13 @@ public class log4J {
         driver.findElement(By.xpath("//input[type='password']")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.cssSelector("button[type='submit']")).submit();
 
+        
+    }
+
+    @Test
+    public void log4jWithPropertiesFile() {
+
+        // Logger log = Logger.getLogger(log4J.class.getName());
         
     }
     
