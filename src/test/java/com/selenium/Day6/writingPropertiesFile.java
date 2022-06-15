844fecd6f3dca959b9ad8e67264832f5e0431a09
VaishnaviDontha <Vaishnavidontha24@gmail.com>
2022-06-15 17:03:07 -0400

    SE Snippets

---
+++ src/test/java/com/selenium/Day6/writingPropertiesFile.java
@@ -0,0 +1,41 @@
+package com.selenium.Day6;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.testng.annotations.Test;
+
+
+public class writingPropertiesFile {
+
+    @Test
+    public void readPropFile() throws IOException {
+
+        FileOutputStream fos = new FileOutputStream("./resources/Properties/config.properties");
+
+        Properties prop = new Properties();
+        prop.setProperty("User1", "Client");
+        prop.setProperty("Environment", "Test");
+
+        prop.store(fos, " ");
+
+    }
+
+    @Test
+    public void readTxtFile() throws IOException {
+
+        FileOutputStream fos = new FileOutputStream("./resources/Properties/config.txt");
+
+        Properties prop = new Properties();
+        
+        prop.setProperty("User1", "Client");
+        prop.setProperty("Environment", "Test");
+
+        prop.store(fos, " ");
+        
+        
+    }
+    
+}
